932d86ba54908919ef6f8ebda1fd6fb29d1ced37
vitorpantoja12 <123208939+vitorpantoja12@users.noreply.github.com>
2024-08-07 20:08:22 -0300

    Update 31.mediap.c

--- 31.mediap.c
+++ 31.mediap.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+int main (){
+float e,f,g,ma;
 
+printf ("\nDigite a primeira nota: ");
+scanf ("%f/\n", &e);
+
+printf ("Digite a segunda nota: ");
+scanf ("%f/\n", &f);
+
+printf ("Digite a terceira nota: ");
+scanf ("%f", &g);
+
+printf ("\nDigite o peso da primeira nota: ");
+scanf ("%f/\n", &p1);
+
+printf ("Digite o peso da segunda nota: ");
+scanf ("%f/\n", &p2);
+
+printf ("Digite o peso da terceira nota: ");
+scanf ("%f", &p3);
+ma=((e*p1)+(f*p2)+(g*p3))/(p1+p2+p3);
+
+printf ("A média ponderada é: %.2f", ma);
+return 0;
+}
